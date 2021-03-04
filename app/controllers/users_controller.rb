b72636cf6d0fa773ff80f4a828fd58a6bb957243
tchartchke <chartai.kl@gmail.com>
2021-03-04 16:33:01 -0600

    commit tree bc8dc8c13ec3442c27ee4302a8e495837b57c605

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to 'index'
+    else
+      redirect_to '/users/new'
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+end
